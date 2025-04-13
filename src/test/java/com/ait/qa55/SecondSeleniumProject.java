fafa1bfb7ff580c6707346f74388e62466a2c7d8
victoriadiakiv <viktoriiadiakivonline@gmail.com>
2025-04-13 18:05:51 +0200

    Add13

---
+++ src/test/java/com/ait/qa55/SecondSeleniumProject.java
@@ -0,0 +1,33 @@
+package com.ait.qa55;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SecondSeleniumProject {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.get("https://demowebshop.tricentis.com");
+
+    }
+
+    @Test
+    public void openDemowebshopTest() {
+        System.out.println("Sales!");
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.close();
+
+    }
+
+
+}
